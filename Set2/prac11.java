b95d5deccd010cfb296bed00f035bd8ce0a50af3
mohitbarad072 <23dcs006@charusat.edu.in>
2024-09-18 13:22:35 +0530

    Add files via upload

---
+++ Set2/prac11.java
@@ -0,0 +1,14 @@
+
+    public class prac11 {
+
+
+        public static void main(String[] args) {
+           
+            String s="CHARUSAT UNIVERSITY";
+            System.out.print("Length  : "+s.length()+"\n");
+            System.out.print("Lower case : "+s.toLowerCase()+"\n");
+            System.out.println(s.replace('H', 'O'));
+            System.out.print("\nName : Mohit Barad_23DCS006 ");
+
+        }
+    }
